6f64b46b32a4b29f4afca30049ea0c45ffa9e3c6
ADMIN <ADMIN@LAPTOP-7RTGLLNR>
2023-07-27 12:14:20 +0530

    FIRST

---
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -0,0 +1,20 @@
+package com.cg.dayone;
+import java.util.*;
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+//		int a=20;
+//		int b=50;
+		System.out.println("Addition: " + (a+b));
+		System.out.println("Subtraction: "+(a-b));
+		System.out.println("Mutiplication: "+(a*b));
+		System.out.println("Division: "+(a/b));
+		System.out.println("Modolus: "+(a%b));
+
+	}
+
+}
